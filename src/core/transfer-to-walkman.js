873fbe897f310572386d5251ecc19f7274c8f813
Jun Gu <2dxgujun@gmail.com>
2018-05-08 09:37:59 +0800

    dev stash

--- src/core/transfer-to-walkman.js
+++ src/core/transfer-to-walkman.js
@@ -15,16 +15,13 @@ function prepareTransfer() {
     if (!mountpoints || mountpoints.length === 0) {
       // prompt no mountpoints found
     } else if (mountpoints.length > 1) {
-      return promptPickMountpoint()
+      return findTargetMountpoint(mountpoints).then(mountpoint => {
+        if (mountpoint) return mountpoint
+        return pickMountpoint(mountpoints)
+      })
     } else {
       return mountpoints[0]
     }
-  })
-}
-
-function resolveMountpoint(mountpoints) {
-  return Promise.filter(mountpoints, mountpoint => {
-    
   })
 }
 
@@ -32,14 +29,26 @@ function getWalkmanGoPath(base) {
   return path.resolve(base, 'MUSIC/WALKMANGO')
 }
 
-function promptPickMountpoint(mountpoints) {
-  inquirer
+function findTargetMountpoint(mountpoints) {
+  return Promise.filter(mountpoints, mountpoint => {
+    return fse.pathExists(getWalkmanGoPath(mountpoint.path))
+  }).then(mountpoints => {
+    if (mountpoints && mountpoints.length === 1) {
+      return mountpoints[0]
+    } else {
+      return null
+    }
+  })
+}
+
+function pickMountpoint(mountpoints) {
+  return inquirer
     .prompt([
       {
         type: 'list',
         name: 'mountpoint',
-        message: 'Pick a mountpoint for transfer playlists',
-        choices: mountpoints
+        message: 'Pick a mountpoint to transfer playlists',
+        choices: mountpoints.map(mountpoint => mountpoint.path)
       }
     ])
     .then(answers => {
@@ -47,32 +56,16 @@ function promptPickMountpoint(mountpoints) {
     })
 }
 
-function findWalkmanDrives() {
-  schedulerId = setInterval(() => {
-    getWalkmanDrives()
-      //.filter(drive => {
-      //  return drive.mountpoints && drive.mountpoints.length > 0
-      //})
-      .then(drives => {
-        if (drives && drives.length > 0) {
-          onAdd(null, device)
-        }
-      })
-  }, 3000)
-}
-
 function findWalkmanMountpoints() {
   return findWalkmanDrives().then(drives => {
     return Promise.filter(drives, drive => {
       return drive.mountpoints && drive.mountpoints.length > 0
     }).then(drives => {
-      const result = []
+      const mountpoints = []
       drives.forEach(drive => {
-        drive.mountpoints.forEach(mountpoint => {
-          result.push(mountpoint.path)
-        })
+        mountpoints.push(...drive.mountpoints)
       })
-      return result
+      return mountpoints
     })
   })
 }

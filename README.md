7899a58dbfa0a92518d3a0f6a32878c822b7cc70
Jun Gu <2dxgujun@gmail.com>
2018-05-10 18:49:20 +0800

    README stash

---
+++ README.md
@@ -0,0 +1,33 @@
+# WalkmanGo
+
+> A CLI for sync online playlists to Sony Walkman deivce
+
+## Features
+
+- Free Hi-Fi music
+- Plug and sync
+-
+
+## Install
+
+```
+$ npm i -g walkman-go
+```
+
+## Example Usage
+
+Firstly, install as a global package.
+
+Create a work directory, for example `~/WalkmanGo`
+
+```
+# walkman-go.ini
+[general]
+workdir = ~/WalkmanGo
+bitrate = flac
+
+[personal]
+uin = 414236069
+playlists[] = Playlist1
+playlists[] = Playlist2
+```
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ini from 'ini'
 import Bluebird from 'bluebird'
 import program from 'commander'
 import Logger from './utils/logger'
+import untildify from 'untildify'
 
 import { schedule as scheduleSync } from './core/schedule-sync'
 import initDetection from './core/init-detection'
@@ -12,17 +13,10 @@ import configureLogger from './core/configure-logger'
 global.Promise = Bluebird
 
 function setup(config) {
-  let { workdir, logdir } = config.general
-  const { bitrate } = config.general
+  const { workdir, logdir, bitrate } = config.general
   const { uin, playlists } = config.personal
-  if (workdir[0] === '~') {
-    workdir = path.join(process.env.HOME, workdir.slice(1))
-  }
-  if (logdir[0] === '~') {
-    logdir = path.join(process.env.HOME, logdir.slice(1))
-  }
-  process.env.walkman_config_workdir = workdir
-  process.env.walkman_config_logdir = logdir
+  process.env.walkman_config_workdir = untildify(workdir)
+  process.env.walkman_config_logdir = untildify(logdir)
   process.env.walkman_config_bitrate = bitrate
   process.env.walkman_config_uin = uin
   process.env.walkman_config_playlists = playlists
@@ -35,11 +29,11 @@ function setup(config) {
 
 program
   .version('0.0.1')
-  .option('-c, --config <path>', 'set config file. defaults to ./walkman.ini')
+  .option('-c, --config <path>', 'set config file. defaults to ./walkman-go.ini')
   .parse(process.argv)
 
 fse
-  .readFile(program.config || './walkman.ini', 'utf-8')
+  .readFile(program.config || './walkman-go.ini', 'utf-8')
   .then(ini.parse)
   .then(setup)
   .then(scheduleSync)

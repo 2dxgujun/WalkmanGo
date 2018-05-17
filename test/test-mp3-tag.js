e383820d4e5547bfd4c4739b5c02e1fe6e2bcae0
Jun Gu <2dxgujun@gmail.com>
2018-05-17 10:38:42 +0800

    stash commit

--- test/test-mp3-tag.js
+++ test/test-mp3-tag.js
@@ -1,17 +1,11 @@
 var ID3v2 = require('node-id3')
-var ID3v1 = require('../src/utils/ID3v1')
+var ID3v1 = require('../src/utils/ID3v1').default
 
-var audio = '/Users/2dxgujun/Desktop/A-Lin - 分手需要练习的.mp3'
+var audio = '/Users/2dxgujun/Desktop/M8000012nFaQ1YGenK.mp3'
 
 ID3v2.write(
   {
-    private: {
-      owner: 'WALKMANGO_BITRATE',
-      data: '320'
-    }
+    title: 'Stronger'
   },
   audio
 )
-
-let tags = ID3v2.read(audio)
-console.log(tags)
